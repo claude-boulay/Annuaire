c95aea2c0d8ec653c5ff5d57f0fbc4fac857fe50
claude-boulay <cbmagala@gmail.com>
2023-03-24 15:45:56 +0100

    try{}catch(){}

--- controllers/controllerEtudiant.php
+++ controllers/controllerEtudiant.php
@@ -5,39 +5,57 @@ require_once("models/model.php");
 
 function addOneEtudiant()
 {
-    if (isset($_POST["enregistrer"])) {
-        $id = $_POST["étudiant_id"];
-        $nom = $_POST["étudiant_nom"];
-        $prenom = $_POST["étudiant_prenom"];
-        $telephone = $_POST["étudiant_tel"];
-        $mail = $_POST["étudiant_mail"];
-        $promo = $_POST["étudiant_promo"];
-        $id_org = $_POST["organisation_id"];
-        $travail = $_POST["travail"];
+    try {
+        if (isset($_POST["enregistrer"])) {
+            $id = $_POST["étudiant_id"];
+            $nom = $_POST["étudiant_nom"];
+            $prenom = $_POST["étudiant_prenom"];
+            $telephone = $_POST["étudiant_tel"];
+            $mail = $_POST["étudiant_mail"];
+            $promo = $_POST["étudiant_promo"];
+            $id_org = $_POST["organisation_id"];
+            $travail = $_POST["travail"];
 
-        $etudiant = new Etudiant($id, $nom, $prenom, $telephone, $mail, $promo, $travail);
-        $etudiant->enregistrer();
+            $etudiant = new Etudiant($id, $nom, $prenom, $telephone, $mail, $promo, $travail);
+            $etudiant->enregistrer();
+
+            if ($travail == 1) {
+
+                $profession = $_POST["profession"];
+                $annee_debut = $_POST["annee_debut"];
+                $annee_fin = $_POST["annee_fin"];
+                $organisation_nom = $_POST["organisation_nom"];
+                $organisation_adresse = $_POST["organisation_adresse"];
+                $organisation_tel = $_POST["organisation_tel"];
+                $organisation_site = $_POST["site"];
 
 
-        $profession = $_POST["profession"];
-        $annee_debut = $_POST["annee_debut"];
-        $annee_fin = $_POST["annee_fin"];
-        $organisation_nom = $_POST["organisation_nom"];
-        $organisation_adresse = $_POST["organisation_adresse"];
-        $organisation_tel = $_POST["organisation_tel"];
-        $organisation_site = $_POST["site"];
+                try {
+                    //appel de la fonction pour ajouter une organisation
+                    addOneOrganisation(
+                        $id_org,
+                        $organisation_nom,
+                        $organisation_adresse,
+                        $organisation_tel,
+                        $organisation_site
+                    );
+                } catch (Exception $e) {
+                    require_once("views/viewEtudiant.php");
+                    echo "<center><h2>L'id de l'Organisation ou le nom ne correspond pas au critère<h2>";
+                }
 
+                try {
+                    addOneTravail($id_org, $id, $profession, $annee_debut, $annee_fin);
+                } catch (Exception $e) {
+                    require_once("views/viewEtudiant.php");
+                    echo "<center><h2>Le champ Travail n'a pas été rempli correctement</h2></center>";
+                }
+            }
+        }
+        require_once("views/viewEtudiant.php");
+    } catch (Exception $e) {
+        require_once("views/viewEtudiant.php");
 
-
-        addOneOrganisation(
-            $id_org,
-            $organisation_nom,
-            $organisation_adresse,
-            $organisation_tel,
-            $organisation_site
-        );
-
-        addOneTravail($id_org, $id, $profession, $annee_debut, $annee_fin);
+        echo " <center><h2>Le champ étudiant à été mal rempli vérifié que l'id n'est pas déjà utilisé</h2></center><br><br>";
     }
-    require_once("views/viewEtudiant.php");
 }

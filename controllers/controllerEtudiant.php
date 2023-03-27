4e10aecaadf4afb722e38270021149765a4d4a8b
claude-boulay <cbmagala@gmail.com>
2023-03-27 15:49:21 +0200

    integrer les valeur a update in view

--- controllers/controllerEtudiant.php
+++ controllers/controllerEtudiant.php
@@ -62,12 +62,12 @@ function addOneEtudiant()
 
 function getUpdate($etudiant_id, $organisation_id)
 {
-    $data = getEtudiant($etudiant_id) + getTravail($etudiant_id, $organisation_id) + getOrganisation($organisation_id);
+    $data = getAll($etudiant_id, $organisation_id);
 
     if (!$data) {
         echo "Aucun étudiant pour cette id";
     } else {
-        require_once('views/viewEtudiantUpdate.php');
+        require_once('views/viewAllUpdate.php');
     }
 
     if (isset($_POST["update"])) {
@@ -78,11 +78,16 @@ function getUpdate($etudiant_id, $organisation_id)
         $mail = $_POST["étudiant_mail"];
         $promo = $_POST["étudiant_promo"];
         $id_org = $_POST["organisation_id"];
-        $travail = $_POST["travail"];
+        if (isset($_POST["profession"])) {
+            $travail = 1;
+        } else {
+            $travail = 0;
+        }
+
 
         $etudiant = new Etudiant($id, $nom, $prenom, $telephone, $mail, $promo, $travail);
         $etudiant->Update();
-        if ($travail == 1) {
+        if ((isset($_POST["update"]))) {
 
             $profession = $_POST["profession"];
             $annee_debut = $_POST["annee_debut"];
@@ -103,22 +108,81 @@ function getUpdate($etudiant_id, $organisation_id)
                     $organisation_site
                 );
             } catch (Exception $e) {
-                require_once("views/viewEtudiant.php");
+                require_once("views/viewAllUpdate.php");
                 echo "<center><h2>L'id de l'Organisation ou le nom ne correspond pas au critère<h2>";
             }
 
             try {
                 UpdateOneTravail($id_org, $id, $profession, $annee_debut, $annee_fin);
             } catch (Exception $e) {
-                require_once("views/viewEtudiant.php");
+                require_once("views/viewAllUpdate.php");
                 echo "<center><h2>Le champ Travail n'a pas été rempli correctement</h2></center>";
             }
+        } else {
+            echo "Tous les champs sont requis";
         }
-    } else {
-        echo "Tous Les champs sont requis";
     }
 }
 
-function getUpdateEtudiant()
+
+function getEtudiantUpdate($etudiant_id)
 {
+    $data = getEtudiant($etudiant_id);
+
+    if (!$data) {
+        echo "Aucun étudiant pour cette id";
+    } else {
+        require_once('views/viewEtudiantUpdate.php');
+    }
+
+    if (isset($_POST["update"])) {
+        $id = $_POST["étudiant_id"];
+        $nom = $_POST["étudiant_nom"];
+        $prenom = $_POST["étudiant_prenom"];
+        $telephone = $_POST["étudiant_tel"];
+        $mail = $_POST["étudiant_mail"];
+        $promo = $_POST["étudiant_promo"];
+        $id_org = $_POST["organisation_id"];
+        if (isset($_POST["profession"])) {
+            $travail = 1;
+        } else {
+            $travail = 0;
+        }
+
+
+        $etudiant = new Etudiant($id, $nom, $prenom, $telephone, $mail, $promo, $travail);
+        $etudiant->Update();
+        if ((isset($_POST["update"]))) {
+
+            $profession = $_POST["profession"];
+            $annee_debut = $_POST["annee_debut"];
+            $annee_fin = $_POST["annee_fin"];
+            $organisation_nom = $_POST["organisation_nom"];
+            $organisation_adresse = $_POST["organisation_adresse"];
+            $organisation_tel = $_POST["organisation_tel"];
+            $organisation_site = $_POST["site"];
+
+
+            try {
+                //appel de la fonction pour ajouter une organisation
+                UpdateOneOrganisation(
+                    $id_org,
+                    $organisation_nom,
+                    $organisation_adresse,
+                    $organisation_tel,
+                    $organisation_site
+                );
+            } catch (Exception $e) {
+                require_once("views/viewEtudiantUpdate.php");
+                echo "<center><h2>L'id de l'Organisation ou le nom ne correspond pas au critère<h2>";
+            }
+
+            try {
+                UpdateOneTravail($id_org, $id, $profession, $annee_debut, $annee_fin);
+            } catch (Exception $e) {
+                require_once("views/viewEtudiantUpdate.php");
+                echo "<center><h2>Le champ Travail n'a pas été rempli correctement</h2></center>";
+            }
+        }
+    }
 }
